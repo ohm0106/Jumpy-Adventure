e9bd166de182e33ba61ce2a572b8887c79296550
ohm0106 <134082781+ohm0106@users.noreply.github.com>
2024-06-12 02:11:39 +0900

    Feat : 플레이어 앞 땅 여부 확인

--- Assets/Main/Scripts/Player.cs
+++ Assets/Main/Scripts/Player.cs
@@ -17,7 +17,12 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     bool isInteractive;
-    
+
+    [SerializeField]
+    float groundCheckDistance = 1.0f;
+    [SerializeField]
+    LayerMask groundLayer;
+
     void Awake()
     {
         controller = GetComponent<CharacterController>();
@@ -27,6 +32,7 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        Debug.Log("controller.isGrounded : " + controller.isGrounded);
         if (controller.isGrounded)
         {
             moveVec = cam.right * inputVec.x + cam.forward * inputVec.y;
@@ -37,13 +43,19 @@ public class Player : MonoBehaviour
                 Quaternion dirQuat = Quaternion.LookRotation(moveVec);
                 Quaternion nextQuat = Quaternion.Slerp(transform.rotation, dirQuat, 0.3f);
                 transform.rotation = nextQuat;
+
+                // 앞쪽에 그라운드가 있는지 확인
+                if (!CanMoveForward(moveVec))
+                {
+                    moveVec = Vector3.zero; // 그라운드가 없으면 이동 멈춤
+                }
             }
         }
     }
 
     void FixedUpdate()
     {
-        controller.SimpleMove(moveVec * speed * Time.deltaTime * 50);   
+        controller.SimpleMove(moveVec * speed * Time.deltaTime * 50);
     }
 
     public void ActionMove(InputAction.CallbackContext context)
@@ -53,9 +65,17 @@ public class Player : MonoBehaviour
 
     public void ActionInteractive(InputAction.CallbackContext context)
     {
-       
     }
 
+    bool CanMoveForward(Vector3 moveDirection)
+    {
+        Vector3 origin = transform.position + controller.center;
+        Vector3 direction = new Vector3(moveDirection.x, -groundCheckDistance, moveDirection.z).normalized;
 
-    
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, groundCheckDistance, groundLayer))
+        {
+            return true; 
+        }
+        return false; 
+    }
 }
